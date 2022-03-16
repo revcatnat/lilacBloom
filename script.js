ef3ce6d480a67c58cf6b832ea80c725f871c5b41
revcatnat <revnathalia@gmail.com>
2022-03-16 11:30:36 +0000

    'editStyle'

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const prodCoords = document.querySelector("#products").getBoundingClientRect();
+const iconsCoords = document.querySelector(".icons-container").getBoundingClientRect();
 const navbar = document.querySelector("header");
 
 window.addEventListener("scroll", function () {
-	if (window.scrollY > prodCoords.top) {
+	if (window.scrollY > iconsCoords.top - 90) {
 		navbar.style.backgroundColor = "white";
 		navbar.style.boxShadow = "0 0.5rem 1rem rgba(0, 0, 0, 0.15)";
 	} else navbar.style.backgroundColor = "transparent";
